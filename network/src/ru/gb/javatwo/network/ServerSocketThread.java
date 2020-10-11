0957648d6d97e76ca38c5dc5c699d0e77aa9e5e3
GremmLeN <GremmLeN@gmail.com>
2020-10-11 23:53:02 +0500

    homework-lesson4

--- network/src/ru/gb/javatwo/network/ServerSocketThread.java
+++ network/src/ru/gb/javatwo/network/ServerSocketThread.java
@@ -4,6 +4,10 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.net.SocketTimeoutException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class ServerSocketThread extends Thread {
 
@@ -11,6 +15,8 @@ public class ServerSocketThread extends Thread {
     private final int timeout;
 
     private final ServerSocketThreadListener listener;
+    private Logger logger = Logger.getLogger(String.valueOf(this.getClass()), getName());
+    ExecutorService executorService = Executors.newCachedThreadPool();
 
     public ServerSocketThread(ServerSocketThreadListener listener, String name, int port, int timeout) {
         super(name);
@@ -18,28 +24,38 @@ public class ServerSocketThread extends Thread {
         this.timeout = timeout;
         this.listener = listener;
         start();
-    }
+        ServerSocketThread serverSocketThread = this;
 
-    @Override
-    public void run() {
-        listener.onServerStart(this);
-        try (ServerSocket server = new ServerSocket(port)) {
-            server.setSoTimeout(timeout);
-            listener.onServerSocketCreated(this, server);
-            while (!isInterrupted()) {
-                Socket client;
-                try {
-                    client = server.accept(); // while (!clientConnected || !timeout) {}
-                } catch (SocketTimeoutException e) {
-                    listener.onServerTimeout(this, server);
-                    continue;
+        executorService.execute(new Runnable() {
+            @Override
+            public void run() {
+                logger.setLevel(Level.ALL);
+                listener.onServerStart(serverSocketThread);
+                logger.log(Level.INFO, "Servers started");
+                try (ServerSocket server = new ServerSocket(port)) {
+                    server.setSoTimeout(timeout);
+                    listener.onServerSocketCreated(serverSocketThread, server);
+                    logger.log(Level.INFO, "Listening to port");
+                    while (!isInterrupted()) {
+                        Socket client;
+                        try {
+                            client = server.accept(); // while (!clientConnected || !timeout) {}
+                        } catch (SocketTimeoutException e) {
+                            listener.onServerTimeout(serverSocketThread, server);
+                            continue;
+                        }
+                        listener.onSocketAccepted(serverSocketThread, server, client);
+                        logger.log(Level.INFO, "Client connected");
+                    }
+                } catch (IOException e) {
+                    listener.onServerException(serverSocketThread, e);
+                    logger.log(Level.WARNING, "Server not started");
+                } finally {
+                    listener.onServerStop(serverSocketThread);
+                    logger.log(Level.INFO, "Server shutdown");
                 }
-                listener.onSocketAccepted(this, server, client);
             }
-        } catch (IOException e) {
-            listener.onServerException(this, e);
-        } finally {
-            listener.onServerStop(this);
-        }
+        });
+
     }
 }
